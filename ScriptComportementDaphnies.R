cde0c3a1a3122252ec4f053cc0d029644aff73c7
CReis93 <100361637+CReis93@users.noreply.github.com>
2023-04-19 23:08:27 +0200

    Update ScriptComportementDaphnies.R

--- ScriptComportementDaphnies.R
+++ ScriptComportementDaphnies.R
@@ -35,9 +35,8 @@ exp = "high_conc_24h"
 if(exp == "low_conc"){ # videos low conc.
 
   conc <- c("C1","C2","C3","C4","C5","C6","C7","C8","C9","C10") # concentrations
-  individus <- c(14,14,8,7,6,9,8,9,14,8) # nbre d'individus
-  thres <- c(-0.115,-0.115,-0.115,-0.115,-0.115,-0.115,-0.115,-0.115,-0.115,-0.18) # seuil
-  images.manq <- c(0,0,0,0,0,0,0,0,0,0) # vct pour image manquante
+  individus <- c(12,14,7,7,6,9,8,9,13,8) # nbre d'individus
+  thres <- c(-0.12,-0.18,-0.16,-0.18,-0.21,-0.21,-0.115,-0.14,-0.18,-0.27) # seuil
   # telecharge les videos low conc.
   list_mov <- list.files(path = "videos/brute", pattern = (".mp4"), full.names = TRUE)
   
@@ -47,8 +46,7 @@ if(exp == "high_conc_24h"){ # videos high conc. 24h
   
   conc <- c("c0","c1","c2","c3","c4") # concentrations
   individus <- c(8,9,9,10,7) # nbre d'individus
-  thres <- c(-0.115,-0.115,-0.115,-0.115,-0.115) # seuil
-  images.manq <- c(0,0,0,0,0) # vct pour image manquante
+  thres <- c(-0.115,-0.08,-0.115,-0.3,-0.3) # seuil
   # telecharge les videos high conc. 24h
   list_mov <- list.files(path = "videos/brute/24H", pattern = (".mp4"), full.names = TRUE)
   
@@ -57,9 +55,8 @@ if(exp == "high_conc_24h"){ # videos high conc. 24h
 if(exp == "high_conc_48h"){ # videos high conc. 48h
   
   conc <- c("c0","c1","c2","c3") # concentrations
-  individus <- c(5,10,10,10) # nbre d'individus
-  thres <- c(-0.115,-0.115,-0.115,-0.115) # seuil
-  images.manq <- c(0,0,0,0) # vct pour image manquante
+  individus <- c(5,9,10,10) # nbre d'individus
+  thres <- c(-0.115,-0.115,-0.07,-0.115) # seuil
   # telecharge les videos high conc. 48h 48H
   list_mov <- list.files(path = "videos/brute/48H", pattern = (".mp4"), full.names = TRUE)
   
@@ -71,11 +68,19 @@ if(exp == "high_conc_48h"){ # videos high conc. 48h
 FramePS <- 15
 
 # creation tableau all_data pour sauvegarder les donnees
-data_all = matrix(ncol = 4)
+data_all = matrix(ncol = 12)
 colnames(data_all) <- c("Concentration",
+                        "distance [mm]",
                         "Vitesse moyenne [mm/s]",
+                        "SD vitesse [mm/s]",
+                        "accélération [mm/s^2]",
                         "Temps d'arret [s]", 
-                        "sinuosite")
+                        "straightness (D/L) [-]",
+                        "alternative straigthness (r) [-]",
+                        "sinuosite [-]",
+                        "Emax [-]",
+                        "DC [-]",
+                        "SDDC [-]")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~ remise à 0 du tableau des données ~~~~~~~~~~~~~~~~~~
 
@@ -129,27 +134,6 @@ for (o in 1:length(conc)){ # commencement de la boucle
   
   print(summary(partIden)) # montre le resultat final pour controle
   
-  resume <- summary(partIden) # sauvegarde le resultat de identifyParticules
-
-  miss <- which(resume$n != individus[o]) # prend les images avec ind. manquant
-  images.manq[o] <- length(miss) # sauvegarde le nbre d'images
-  N.images <- N.images[-c(miss)] # enleves les images avec des individus en trop
-  
-  if(resume$particles[2] != 0){
-
-    # On recommence tout sans les images "defectueuses" ........................
-  
-    allFullImages <- loadImages(dirPictures = "videos/images",
-                              nImages = N.images)
-    stillBack <- createBackground(allFullImages,method="mean")
-    allImages <- subtractBackground(stillBack)
-    partIden <- identifyParticles(allImages,
-                                threshold = thres[o],
-                                pixelRange = c(5,500)) # reporter la valeur seuile
-    print(summary(partIden)) # montre le resultat final pour controle
-  
-  }
-  
   #~~~~~~~~~~~~~~~~~~~~~~~~~ Enregistrement des mouvements ~~~~~~~~~~~~~~~~~~~~~
 
   records <- trackParticles(partIden,L=200,R=3) # traque et lie les points. 
@@ -157,109 +141,160 @@ for (o in 1:length(conc)){ # commencement de la boucle
   #~~~~~~~~~~~~~~~~~~~ créer l'image de suivit et la video ~~~~~~~~~~~~~~~~~~~~~
   
   setwd("path")
-  ext <- ".pdf"
-  name <- "imageSuivis_C"
-  num <- as.character(o)
-  fichier <- paste(name,num,ext,sep="")
-  pdf(file = fichier)
+  pdf(file = paste("imageSuivis_C",as.character(o),".pdf",sep=""))
   par(mfrow=c(1,1))
   plot(records,type="trajectories",incThres=1) # affichage du resultat
   dev.off()
   setwd("path")
 
-  #plot(records,type="animation",incThres=1,
-       #path="resultats/videoChemin/",
-       #libavpath = "libav/usr/bin/avconv.exe") # creation video de suivis
+  plot(records,type="animation",incThres=1,
+       path="resultats/videoChemin/",
+       libavpath = "libav/usr/bin/avconv.exe") # creation video de suivis
+  file.rename(from="resultats/videoChemin/animation.mp4", 
+              to = paste("resultats/videoChemin/",as.character(o),".mp4",sep="")) # renome le fichier
   
-  #name <- as.character(o) # trasnforme le numero de la video en caractere
-  #ext <- ".mp4" # creation de l'extension
-  #chemin <- "resultats/videoChemin/" # creation du chemin
-  #fichier <- paste(chemin,name,ext,sep="") # merge des trois string
+  #~~~~~~~~~~~~~~~~~~~~~~~ Enregistrement des coordonnées ~~~~~~~~~~~~~~~~~~~~~~
   
-  #file.rename(from="resultats/videoChemin/animation.mp4", to = fichier) # renome le fichier
+  coordonnes <- matrix(ncol = (individus[o]*2), nrow = (length(records$trackRecord[1,,1]))) # creation d'une matrice coordonnees
+  
+  wu <- 1
+  wuu <- 1
+  
+  for(i in 1 : (individus[o]*2)){
+    
+    if(wuu==3){wu <- wu + 1}
+    if(wuu==3){wuu <- 1}
+    
+    coordonnes[,i] <- records$trackRecord[wu,,wuu]
+    
+    wuu <- wuu + 1
+    
+  }
+  
+  if(o==1){data_coords = matrix(nrow = compteur)} # créer le tableau pour les coordonnees
+  data_coords <- cbind(data_coords,coordonnes) # rajoute les donnees aux tableaux
   
   #~~~~~~~~~~~~~~~~~~~~~~~ Enregistrement des donnees ~~~~~~~~~~~~~~~~~~~~~~~~~~
   
   resultat <- summary(records, incThres = 1)
   list_data <- Map(as.data.frame, resultat) # extrait les donnees
-  Move <- as.data.frame(list_data$particles$`Total movement`) # isole les donnees
   
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~ converstion en vitesse ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-  Move <- Move*4 # mise à l'echelle 100% 
-  Move <- Move*10/125.6 # conversion pixels en mm (10mm = 125.6 pix)
-  Vit <- Move/180 # conversion en vitesse (mm/s) avec video de 3min (180 sec.)
-  
-  #~~~~~~ donnees utile pour le temps d'arret et sinuosite ~~~~~~~~~~~~~~~~~~~~~
+  #~~~~~~~ donnees utile pour les traits comportemantales ~~~~~~~~~~~~~~~~~~~~~~
   
   nbre <- summary(records,incThres=1)$N # enregistre le nbre d'individus
-  step <- compteur-length(miss) # enregistre le nombre d'images (steps)
   Place <- records$trackRecord # enregistre les coordonnees x/y
   
-  # creation des tableau pour sauvegarde des resultats
-  Tab_arret = matrix(ncol = 1, nrow = nbre) # creation d'une matrice temps d'arret
-  Tab_sin = matrix(ncol = 1, nrow = nbre) # creation d'une matrice sinuosite
-  
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~ calcul du temps d'arret ~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #~~~~~~~~~~~~~~~~~~~~~~~~~ enregistrement distance ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  for (l in 1: nbre){
+  dist <- as.matrix(list_data$particles$`Total movement`) # isole les donnees
+  dist <- dist*4*10/125.6 # mise à l'echelle 100% et convertion pixels en mm (10mm = 125.6 pix)
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~ donnes avec trajr ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  #initialisation des tableau pour enregistrer les donnees
+  Tab_vit = matrix(ncol = 1, nrow = nbre) # vitesse
+  Tab_sd = matrix(ncol = 1, nrow = nbre) # sd speed
+  Tab_acc = matrix(ncol = 1, nrow = nbre) # acceleration
+  Tab_arret = matrix(ncol = 1, nrow = nbre) # temps d'arret
+  Tab_strait = matrix(ncol = 1, nrow = nbre) # stabilite (D/L)
+  Tab_r = matrix(ncol = 1, nrow = nbre) # stabilite relative (r)
+  Tab_sin = matrix(ncol = 1, nrow = nbre) # sinuosite
+  Tab_Emax = matrix(ncol = 1, nrow = nbre) # Emax
+  Tab_DC = matrix(ncol = 1, nrow = nbre) # DC
+  Tab_SDDC = matrix(ncol = 1, nrow = nbre) # SDDC
+  
+  
+  for (u in 1 : nbre){
     
     # Definition de x, y, et temp
+    coords <- data.frame(x = Place[c(u),,c(1)], # x
+                         y = Place[c(u),,c(2)], # y
+                         time = seq((180/compteur), 180, by = 180/compteur)) # temps
     
-    coords <- data.frame(x = Place[c(l),,c(1)], # x
-                         y = Place[c(l),,c(2)], # y
-                         time = seq((180/step), 180, by = 180/step))# temps
+    # création de la trajectoire 1 (pour : vitesse, sd vitesse, acceleration, temps d'arret)
+    trj1 <- TrajFromCoords(coords, spatialUnits = "pixels", timeCol = 3)
+    # Smooth before calculating derivatives
+    smoothed <- TrajSmoothSG(trj1)
     
-    # mise a l'echelle de pixel en metre
-    trj <- TrajFromCoords(coords, spatialUnits = "pixels")
+    # création de la trajectoire 2 (pour :stabilité (D/L), stabilite relative (r), sinuosite, Emax, DC, SDDC)
+    trj2 <- lapply(1:1, function(i) TrajFromCoords(coords, spatialUnits = "pixels", timeCol = 3))
+    # rediscretisation de la trajectoire
+    reds <- lapply(1, function(ss) lapply(1:1, function(i) TrajRediscretize(trj2[[i]], ss)))
+    
+    #~~~~~~~~~~~~~~~~~~~~~ calcul vitesse et acceleration ~~~~~~~~~~~~~~~~~~~~~~
 
-    #  10 mm (=   1 cm) =  125.6 pixels dans la video
-    trj <- TrajScale(trj, 10 / 125.6, "mm")
-
-    # analyse de la trajectoire
+    # Calculate speed and acceleration
+    derivs <- TrajDerivatives(smoothed)
+    # sauvegarde les données
+    Tab_vit[u] <- mean(derivs$speed*4*10/125.6) # vitesse
+    Tab_sd[u] <- sd(derivs$speed*4*10/125.6) # vitesse dc
+    Tab_acc[u] <- mean(derivs$acceleration*4*10/125.6) # acceleration
     
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~ calcul du temps d'arret ~~~~~~~~~~~~~~~~~~~~~~~~
     # Calcul des intervals
-    intervals <- TrajSpeedIntervals(trj, slowerThan = 0.05) # en mm/s
+    intervals <- TrajSpeedIntervals(smoothed, slowerThan = 0.157) # en pix/s
     
-    #plot(intervals)
-    Tab_arret[l] <- sum(intervals$duration)
-  }
+    Tab_arret[u] <- sum(intervals$duration) # temps d'arret
   
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ sinuosite ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-  for (m in 1: nbre){
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~ stabilite (D/L) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
-    # Definition de x, y, et temp
-    coords <- data.frame(x = Place[c(m),,c(1)], # x
-                         y = Place[c(m),,c(2)], # y
-                         time = seq((180/step), 180, by = 180/step)) # temps
-  
-    # mise a l'echelle de pixel en metre
-    trj <- TrajFromCoords(coords, spatialUnits = "pixels")
-  
-    # Calcule le changement de direction
-    Tab_sin[m] <- TrajSinuosity2(trj)
-  
+    Tab_strait[u] <- sapply(reds, function(rtrjs) sapply(1:1, function(i) TrajStraightness(trj2[[i]]))) # D/L
+    Tab_r[u] <- sapply(reds, function(rtrjs) sapply(1:1, function(i) Mod(TrajMeanVectorOfTurningAngles(trj2[[i]])))) # r
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ sinuosite ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Tab_sin[u] <- sapply(reds, function(rtrjs) sapply(1:1, function(i) TrajSinuosity2(trj2[[i]]))) # sinuosite
+    
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Emax ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Tab_Emax[u] <- sapply(reds, function(rtrjs) sapply(1:1, function(i) TrajEmax(trj2[[i]]))) # Emax
+    
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DC ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Tab_DC[u] <- sapply(1:1, function(i) mean(TrajDirectionalChange(trj2[[i]])))
+    Tab_SDDC[u] <- sapply(1:1, function(i) sd(TrajDirectionalChange(trj2[[i]])))
+    
   }
   
   #~~~~~~~~~~~~~~~~~~~~~~~~Compilation de toutes les donnees~~~~~~~~~~~~~~~~~~~~
   
-  data <- cbind(conc[o],Vit,Tab_arret,Tab_sin) # transforme les donnees en DF
-  colnames(data) <- c("Concentration","Vitesse moyenne [mm/s]","Temps d'arret [s]", "sinuosite")
+  data <- cbind(conc[o],
+                dist,
+                Tab_vit,
+                Tab_sd,
+                Tab_acc,
+                Tab_arret,
+                Tab_strait,
+                Tab_r,
+                Tab_sin,
+                Tab_Emax,
+                Tab_DC,
+                Tab_SDDC) # transforme les donnees en DF
+  
+  colnames(data) <- c("Concentration",
+                      "distance [mm]",
+                      "Vitesse moyenne [mm/s]",
+                      "SD vitesse [mm/s]",
+                      "accélération [mm/s^2]",
+                      "Temps d'arret [s]", 
+                      "straightness (D/L) [-]",
+                      "alternative straigthness (r) [-]",
+                      "sinuosite [-]",
+                      "Emax [-]",
+                      "DC [-]",
+                      "SDDC [-]")
+  
   data_all <- rbind(data_all,data) # rajoute les donnees aux tableaux
   
   if ( o == length(conc)){ 
     
+    data_coords <- data_coords[,-1] # supprime la premiere colonne (NA)
     data_all <- data_all[-1,] # supprime la premiere ligne (NA)
     row.names(data_all) <- c(1:length(data_all[,1])) # change les index des lignes
     
-    # Sortie images manquante et pourcentage
-    images.manq.pour <- images.manq/compteur*100
-    donne.manq <- cbind(images.manq,images.manq.pour)
-    
     setwd("path")
     write.csv(data_all,file="donnees_comportements.csv")
-    write.csv(donne.manq,file="imagesManquante.csv")
+    write.csv(data_coords,file="coordonnes.csv")
     setwd("path")
     
   }
